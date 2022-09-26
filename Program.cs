d1b573cb179d2eb414604e6d60d57a9e4cbe5a1d
KerinAO <90246117+KerinAO@users.noreply.github.com>
2022-09-26 10:53:00 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InicijalnoPonavljanjeZadatak6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int brojClanova;
+            double suma = 0;
+            double broj = 1;
+
+            Console.WriteLine("Unesi broj članova reda: ");
+            brojClanova = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < brojClanova; i++)
+            {
+                broj = broj * 3;
+                suma = suma + (1 / broj);
+            }
+
+            Console.WriteLine(suma);
+            Console.ReadKey();
+        }
+    }
+}
